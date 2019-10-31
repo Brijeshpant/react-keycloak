9b5faf275808c842d62699b7a4230abd823a4a29
Brijesh Pant <bpant@xebia.com>
2019-10-31 16:15:43 +0530

    Created app with key cloak auth

--- src/index.js
+++ src/index.js
@@ -2,11 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Keycloak from "keycloak-js";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let keycloakConfig = {
+    "realm": "demo",
+    "url": "http://localhost:8081/auth",
+    "ssl-required": "external",
+    "resource": "login-app",
+    "public-client": true,
+    "confidential-port": 0,
+    clientId: 'login-app', 
+    onLoad: 'login-required'
+  }
+
+let keycloak = Keycloak(keycloakConfig);
+
+keycloak.init({ onLoad: keycloakConfig.onLoad }).success((auth) => {
+
+    if (!auth) {
+        window.location.reload();
+    } else {
+        console.info("Authenticated");
+    }
+
+    ReactDOM.render(<App />, document.getElementById('root'));
+
+    localStorage.setItem("react-token", keycloak.token);
+    localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+
+    setTimeout(() => {
+        keycloak.updateToken(30).success((updated) => {
+            console.log('token updated' + updated);
+        }).error(() => {
+            console.error('failed');
+        });
+
+
+    }, 60000)
+
+}).error(() => {
+    console.error("unsuccesful");
+});
